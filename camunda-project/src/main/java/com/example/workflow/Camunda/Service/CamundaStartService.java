7079373cc9435091e704ff2d967ca27debcc7797
oussama <oussamakifo@gmail.com>
2022-09-06 20:59:11 +0000

    Save Variables inside msgResult and insert them into Database.Users

--- camunda-project/src/main/java/com/example/workflow/Camunda/Service/CamundaStartService.java
+++ camunda-project/src/main/java/com/example/workflow/Camunda/Service/CamundaStartService.java
@@ -2,21 +2,23 @@ package com.example.workflow.Camunda.Service;
 
 import com.example.workflow.Modals.User;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResultWithVariables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CamundaStartService {
     @Autowired
     RuntimeService runtimeService;
 
-    public void startProcessByMessage(User user)
+    public List<MessageCorrelationResultWithVariables> startProcessByMessage(User user)
     {
-        runtimeService.createMessageCorrelation("msgRecieveUser")
-                .setVariable("name",user.getName())
-                .setVariable("age",user.getAge())
-                .setVariable("can_enter",user.isCanEnter())
-                .correlate();
+        return runtimeService.createMessageCorrelation("msgRecieveUser")
+               .setVariable("name",user.getName())
+               .setVariable("age",user.getAge())
+               .correlateAllWithResultAndVariables(true);
     }
 
 }
--- camunda-project/src/main/java/com/example/workflow/Controller/UserController.java
+++ camunda-project/src/main/java/com/example/workflow/Controller/UserController.java
@@ -3,8 +3,11 @@ package com.example.workflow.Controller;
 import com.example.workflow.Modals.User;
 import com.example.workflow.Camunda.Service.CamundaStartService;
 import com.example.workflow.Service.UserService;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResultWithVariables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -18,8 +21,13 @@ public class UserController {
     @RequestMapping(value="/add",method = RequestMethod.POST)
     public void saveAge(@RequestBody User user)
     {
-        camundaStartService.startProcessByMessage(user);
-        userService.addUser(user);
+     //   msgResult.get(0).getVariables().get("canEnter")
+        List<MessageCorrelationResultWithVariables> msgResult = camundaStartService.startProcessByMessage(user);
+        User finalUser = new User();
+        finalUser.setName(String.valueOf(msgResult.get(0).getVariables().get("name")));
+        finalUser.setAge((Long) msgResult.get(0).getVariables().get("age"));
+        finalUser.setCanEnter((Boolean) msgResult.get(0).getVariables().get("canEnter"));
+        userService.addUser(finalUser);
 
     }
 }
